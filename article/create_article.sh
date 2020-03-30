c0526942f3481dc42d6c3b0362cafcd76b02d76b
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-30 14:01:19 +0200

    Use 'babette_article' as main file, that is, where the content is

--- article/create_article.sh
+++ article/create_article.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-pdflatex babette_article.tex --shell-escape
-bibtex babette_article.aux
-pdflatex babette_article.tex --shell-escape
-pdflatex babette_article.tex --shell-escape
+pdflatex babette_header --shell-escape
+bibtex babette_header
+pdflatex babette_header --shell-escape
+pdflatex babette_header --shell-escape
+mv babette_header.pdf babette_article.pdf
 
 # pdfunite babette_article.pdf ../figures/figures.pdf article_full.pdf
