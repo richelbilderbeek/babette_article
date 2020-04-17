31db5c6086a32c1fa2b113855e4f7a9f9c218769
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-17 13:40:13 +0200

    Clean

--- article/clean.sh
+++ article/clean.sh
@@ -7,4 +7,5 @@
 #   ./clean.sh
 #
 # All except the tex, bst and bib file
-rm -f *.aux *.bbl *.bcf *.blg *.doc *.docx *.log *.out *.pdf *.rtf *.run.xml *.txt
+rm -f *.aux *.bbl *.bcf *.blg *.doc *.docx *.log *.out *.pdf *.rtf *.run.xml *.txt *.bak
+
